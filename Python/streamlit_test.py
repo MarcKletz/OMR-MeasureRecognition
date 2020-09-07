d043d1c13d7f87f79e2794d8788464e09cd41eaa
Marc Kletz <mkletz.mmt-m2018@fh-salzburg.ac.at>
2020-09-07 18:18:00 +0200

    removing detectron2 imports from the test file

--- Python/streamlit_test.py
+++ Python/streamlit_test.py
@@ -4,13 +4,6 @@ import PIL
 import os, json, cv2, random
 from IPython import display
 
-import detectron2
-from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
-from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog, DatasetCatalog
-
 def main():
 	st.write("HELLO WORLD")
 	
