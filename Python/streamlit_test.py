02418ddf318f234aea8f1d24f395ed069aa57323
Marc Kletz <mkletz.mmt-m2018@fh-salzburg.ac.at>
2020-09-09 20:03:21 +0200

    adding import detectron2 to test file

--- Python/streamlit_test.py
+++ Python/streamlit_test.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+import detectron2
+
+
 """
 # Streamlit test
 by Marc Kletz
@@ -8,6 +11,11 @@ by Marc Kletz
 
 def main():
 	st.write("Streamlit is working!")
+	
+	test_detectron()
+	
+def test_detectron():
+	
 
 if __name__ == "__main__":
 	main()
