896e0c445729a6c5fb8df9fbeb3ab83cc629e145
Marc Kletz <mkletz.mmt-m2018@fh-salzburg.ac.at>
2020-09-09 20:28:39 +0200

    fixing indents

--- Python/streamlit_test.py
+++ Python/streamlit_test.py
@@ -25,17 +25,17 @@ def main():
 def test_detectron():
 	image = Image.open("./Images/detectron_test_image.jpg").convert("RGB")
 	st.image(image, "Your uploaded image", use_column_width=True)
-	
+
 	cfg = get_cfg()
 	cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
 	cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
 	cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
 	predictor = DefaultPredictor(cfg)
 	outputs = predictor(im)
-	
+
 	v = CustomVisualizer(im[:, :, ::-1], scale=1)
-    v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-    st.image(v.get_image()[:, :, ::-1], "Network prediction", use_column_width=True)
+	v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+	st.image(v.get_image()[:, :, ::-1], "Network prediction", use_column_width=True)
 
 if __name__ == "__main__":
 	main()
