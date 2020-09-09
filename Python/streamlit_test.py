50ba0e20906bf42b68d053b22b6f9e521c34f68b
Marc Kletz <mkletz.mmt-m2018@fh-salzburg.ac.at>
2020-09-09 20:27:30 +0200

    adding more stuff to streamlit test

--- Python/streamlit_test.py
+++ Python/streamlit_test.py
@@ -1,7 +1,7 @@
-import cv2
+from PIL import Image
 import streamlit as st
 
-from ImageDisplayer import ImageDisplayer
+from CustomVisualizer import CustomVisualizer
 
 import detectron2
 from detectron2 import model_zoo
@@ -23,8 +23,19 @@ def main():
 	test_detectron()
 	
 def test_detectron():
-	im = cv2.imread("../Images/detectron_test_image.jpg")
-	ImageDisplayer().cv2_imshow(im)
+	image = Image.open("./Images/detectron_test_image.jpg").convert("RGB")
+	st.image(image, "Your uploaded image", use_column_width=True)
+	
+	cfg = get_cfg()
+	cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+	cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+	cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+	predictor = DefaultPredictor(cfg)
+	outputs = predictor(im)
+	
+	v = CustomVisualizer(im[:, :, ::-1], scale=1)
+    v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    st.image(v.get_image()[:, :, ::-1], "Network prediction", use_column_width=True)
 
 if __name__ == "__main__":
 	main()
